1dd6ce5d778fa5be4be4d7089045f5cf9ba79391
colemyhre <colemyhre6@gmail.com>
2024-03-03 14:13:59 -0800

    Create darkenColor funtcion to use on mousover of each div

--- script.js
+++ script.js
@@ -24,7 +24,15 @@ btn.addEventListener('click', () => {
         box.style.height = boxHeight+'px';
         box.style.width = boxHeight+'px';
         box.addEventListener('mouseover', () => {
-            box.style.backgroundColor = 'hsl(120, 90%, 10%)';
+            box.style.backgroundColor = darkenColor(box);
+            // let checkColor = getComputedStyle(box).backgroundColor;
+            // checkColor = checkColor.substring(5,checkColor.length-1);
+            // checkColor = checkColor.replace(/ /g, '');
+            // checkColor = checkColor.split(',');
+            // console.log(checkColor);
+            // let sat = checkColor[3];
+            // console.log(sat);
+            //box.style.backgroundColor = 'hsl(120, 90%, 90%)';
         })
         container.appendChild(box);
     }
@@ -36,5 +44,33 @@ function  clearDivs(parent) {
     }
 }
 
+function darkenColor(box) {
+    let checkColor = getComputedStyle(box).backgroundColor;
+    let color = checkColor.substring(5,checkColor.length-1);
+    color = color.replace(/ /g, '');
+    color = color.split(',');
+    let rVal = color[0];
+    let gVal = color[1];
+    let bVal = color[2];
+    let sat = color[3];
+    console.log(sat);
+    sat = Number(sat);
+    //sat = sat.toFixed(0);
+    console.log(sat);
+    
+    if (sat == 1) {
+        console.log(checkColor);
+        return checkColor;
+    }
+    else if (sat == 0.9) {
+        newColor = 'rgba('+rVal+', '+gVal+', '+bVal+', '+1+')';
+        return newColor;
+        console.log(newColor);
+    }
+    else {
+        newColor = 'rgba('+rVal+', '+gVal+', '+bVal+', '+(sat+0.1)+')';
+        return newColor;
+    }
+}
 
 
